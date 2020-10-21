92f44d7ac82de3b64792486243c0f35de228678d
zpawlo02 <zpawlo02@gmail.com>
2020-10-21 11:30:26 +0200

    Initial Commit

---
+++ algos4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algos4
+//
+//  Created by Paweł Zieliński on 21/10/2020.
+//  Copyright © 2020 Paweł Zieliński. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
